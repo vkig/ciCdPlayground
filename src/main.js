1da8a577586a63f61bcf91e06627a4f9ec2640d5
Paul Käufl <kaeufl@users.noreply.github.com>
2024-05-02 17:49:16 +0200

    add my introduction

--- src/main.js
+++ src/main.js
@@ -4,25 +4,26 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Paul Käufl',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Red, green and yellow'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'How to improve the workshop.'
 		],
 		// What do you like to do when you're not coding?
-		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+		hobbies: [ 
+			'play [with my daughter|piano|(computer) games]', 
+			'read a book',
+			'cook a meal',
+			'go on a hiking trip'
 		]
 	}
 });
