ffbe6409fbbf948d410de9ac67e6c8fe7e0de85d
Paul Käufl <paul.kaeufl@tngtech.com>
2025-05-16 14:18:35 +0200

    Revert accidental commit

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "Christof Happ",
+    name: "James Butler",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["red, green and yellow", "fast feedback", "iterative development process"],
+    associations: ["Ehm, what?"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins",
+    tools: "Jenkins, maybe?",
     // What do you want to learn in this workshop?
-    expectations: ["How to improve the workshop.", "Which CI/CD Tools do you use in your projects?"],
+    expectations: ["what you are talking about"],
   },
 });
