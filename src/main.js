22e1017911d7a64d73bbd6b22450620f915524c2
Gergely Pákozdi <gergely.pakozdi@tngtech.com>
2025-08-22 09:07:45 +0200

    Personal details added in main

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Gergely Pákozdi",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["GhActions", "Jenkins", "Cloud", "Infrastructure"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: ["Jenkins", "GhActions", "ArgoCD"],
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Have a better overview about the tools and the idea."],
   },
 });
